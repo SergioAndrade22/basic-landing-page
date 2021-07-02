310c73d7e044058c22df2f2087343910e1b103b7
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-07-02 14:08:39 -0300

    Created interfaced for type compliance

---
+++ src/models/Item.ts
@@ -0,0 +1,11 @@
+export default class Item extends Object {
+    id: number
+    cost: number
+    cuisine: string
+    hasAlcohol: boolean
+    isCoeliac: boolean
+    isVegan: boolean
+    isVege: boolean
+    name: string
+    picture: string
+}
